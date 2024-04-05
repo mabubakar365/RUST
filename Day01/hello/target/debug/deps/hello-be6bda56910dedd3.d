e3f9fd7f9038f056dca99c9256e69363a30eed23
mabubakar365 <m.abubakar365@yahoo.com>
2024-04-05 15:55:43 +0500

    Added code for Day01

---
+++ Day01/hello/target/debug/deps/hello-be6bda56910dedd3.d
@@ -0,0 +1,5 @@
+/home/embeddedmaster/Desktop/RUST/Day01/hello/target/debug/deps/hello-be6bda56910dedd3: src/main.rs
+
+/home/embeddedmaster/Desktop/RUST/Day01/hello/target/debug/deps/hello-be6bda56910dedd3.d: src/main.rs
+
+src/main.rs:
---
+++ Day01/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/embeddedmaster/Desktop/RUST/Day01/hello/target/debug/hello: /home/embeddedmaster/Desktop/RUST/Day01/hello/src/main.rs
